1604640cf7945fb7dd7ab1b1e71e6f2f8ee8eddc
baechu8716 <baechu8716@naver.com>
2025-03-20 16:22:24 +0900

    [level 0] Title: 편지, Time: 0.14 ms, Memory: 31.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120898. 편지/편지.cs
@@ -0,0 +1,9 @@
+using System;
+
+public class Solution {
+    public int solution(string message) {
+        int length = message.Length;
+        int answer = 2 * length;
+        return answer;
+    }
+}
